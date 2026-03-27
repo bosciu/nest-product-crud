64884fbac69ded105f13ed3edcb2d84f7aebf873
Giovanni Bertolini <giovannibertolinidev@gmail.com>
2026-03-27 18:37:44 +0100

    Completed products CRUD // Handled reponse errors

---
+++ backend/src/products/products.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductStockDto } from './dto/update-product-stock.dto';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  create(@Body() dto: CreateProductDto) {
+    return this.productsService.create(dto);
+  }
+
+  @Get()
+  findAll(@Query() { page, limit }: PaginationDto) {
+    return this.productsService.findAll(page, limit);
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.findOne(id);
+  }
+
+  @Patch('/update-stock/:id')
+  updateStock(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateProductStockDto,
+  ) {
+    return this.productsService.updateStock(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.delete(id);
+  }
+}
